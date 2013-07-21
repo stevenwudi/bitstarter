9a0af9bf462f0ae1d50ad7b1ff2451387bd72d29
Stevenwudi <stevenwudi@gmail.com>
2013-07-21 19:22:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs=require('fs');
 
-var file='index.hmtl';
+var file='index.html';
 
 var buffer = new Buffer(fs.readFileSync(file));
 
