9690cc59e86f0f5738a55a1ef01471f357b2699d
Stevenwudi <stevenwudi@gmail.com>
2013-07-21 18:46:44 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
-//var buffer = new Buffer(fs.readFileSync('index.html'));
+var buffer = new Buffer(fs.readFileSync('index.html'));
 
 
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-response.send("Hello world");};
-//response.send(buffer.toString('utf-8'));
+//response.send("Hello world");};
+response.send(buffer.toString('utf-8'));
+});
 
 
 var port = process.env.PORT || 5000;
