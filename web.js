98f3fe50a97c2d974bb84d23f2189d368f43203d
stevenwudi <stevenwudi@gmail.com>
2013-10-05 15:39:53 +0100

    new

--- web.js
+++ web.js
@@ -16,7 +16,7 @@ response.send(buffer.toString());
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
